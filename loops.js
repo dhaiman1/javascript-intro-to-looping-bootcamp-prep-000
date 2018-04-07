e8dd630847fc64bf8b192f6ecbaaba3036f8ae60
David Haiman <dhaiman2@gmail.com>
2018-04-07 22:29:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,6 +20,9 @@ function maybeTrue() {
 }
 
  function doWhileLoop(array) {
+   function maybeTrue() {
+   return Math.random() >= 0.5
+   }
    do {
      array.pop;
    } while (array.length > 0 && maybeTrue());
